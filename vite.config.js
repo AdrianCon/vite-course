a4d769c1b079937e51ceb001ca0bf2a983a827aa
AdrianCon <adrian.dev@tutanota.com>
2024-03-23 23:02:38 -0600

    Dive into simple vite config options"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // Alias the @ to /src
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    assetsInlineLimit: 4096,
+    cssCodeSplit: true, // Make sure css is extracted into a separate file,
+    sourcemap: "hidden", // Useful for application monitoring, but not shown to the user
+  },
 });
