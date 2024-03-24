b981cd8948fc48fbf313714bbc1cdf921ac52e01
AdrianCon <adrian.dev@tutanota.com>
2024-03-23 22:10:12 -0600

    Import example css module to App component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "./components/HelloWorld.vue";
+import style from "./assets/example.module.css";
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 :class="style.example">Hello CSS Modules</h1>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
